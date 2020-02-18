c1ef58809c25915d92a754f6272b00753f445f38
Willian Duarte de Andrade <willianduarte.me@gmail.com>
2020-02-18 13:53:00 -0300

    OrderIssue model

--- desafio-03/src/database/index.js
+++ desafio-03/src/database/index.js
@@ -5,10 +5,11 @@ import Recipient from '../app/models/Recipient'
 import File from '../app/models/File'
 import Deliveryman from '../app/models/Deliveryman'
 import Order from '../app/models/Order'
+import OrderIssue from '../app/models/OrderIssue'
 
 import dbConfig from '../config/database'
 
-const models = [User, Recipient, File, Deliveryman, Order]
+const models = [User, Recipient, File, Deliveryman, Order, OrderIssue]
 
 class Database {
   constructor() {
